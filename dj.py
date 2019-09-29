cbfe974d174a018ec410b8da2f40c0f73cd125e2
Gabriel <gabriel.gimenez98@gmail.com>
2019-09-29 17:54:03 -0300

    add função que transforma a lista inserida num dicionario para fazer o grafo"

--- dj.py
+++ dj.py
@@ -1,21 +1,21 @@
-grafo = { "A" : { "B" : 1, "C":2 },
-          "B" : { "D":2, "E":4 },
-          "C" : { "E":2 },
-          "D" : { "F": 6 },
-          "E" : { "F": 7 },
-          "F" : { }
-          }
+# grafo = { "A" : { "B" : 1, "C":2 },
+#           "B" : { "D":2, "E":4 },
+#           "C" : { "E":2 },
+#           "D" : { "F": 6 },
+#           "E" : { "F": 7 },
+#           "F" : { }
+#           }
 
-grafo2 = {  "D" : { "A": 4, "H": 1 },
-            "A" : { "H": 10, "E": 1 },
-            "H" : { "E": 5, "I": 9 },
-            "E" : { "F" : 3 },
-            "I" : { "J" : 2 },
-            "F" : { "I" : 1, "G": 7, "B": 1, "C": 3 },
-            "G" : { },
-            "J" : { "G" : 1 },
-            "B" : { "C" : 2 },
-            "C" : { } }
+# grafo2 = {  "D" : { "A": 4, "H": 1 },
+#             "A" : { "H": 10, "E": 1 },
+#             "H" : { "E": 5, "I": 9 },
+#             "E" : { "F" : 3 },
+#             "I" : { "J" : 2 },
+#             "F" : { "I" : 1, "G": 7, "B": 1, "C": 3 },
+#             "G" : { },
+#             "J" : { "G" : 1 },
+#             "B" : { "C" : 2 },
+#             "C" : { } }
             
 
 def dijkstra_path(grafo, origem, fim): #retorna a menor distancia de um No origem até um No destino e o caminho até ele
@@ -66,8 +66,65 @@ def input_data(tamanho_grafo):
 
     return [lst_full[x] for x in range(tamanho_grafo)]
 
-# dijkstra_path(grafo,"A","F")
+def transforma_grafo(lista_vertices):
 
-tamanho_grafo = int(input("Digite a quantidade de vertices de o grafo tera\n"))
+    grafo = {}
+    grafo = {}
+
+    for item in lista_vertices:
+        grafo[item] = {}
+
+
+
+    for i in range(len(lista_vertices) -1):
+        dicionario = {}
+        if i % 2 == 0:
+            par = True
+            distancia = 3
+        else:
+            par = False
+            distancia = 2
+
+        for j in range(distancia):
+            if j + i + 1 < len(lista_vertices):
+                resp = input(f'{lista_vertices[i]} faz ligação com {lista_vertices[j + i + 1]}?\n S = sim N = não\n')
+                resp = resp.upper()
+                if resp == 'S':
+
+                    if par:
+                        if (j + i + 1) - i == 3 :
+                            aresta = 1.41
+                        else:
+                            aresta = 1
+
+                    else:
+                        if (j + i + 1) - i == 1 :
+                            aresta = 1.41
+                        else:
+                            aresta = 1
+
+                    dicionario[lista_vertices[j + i + 1]] = aresta
+            else:
+                break
+
+        grafo[lista_vertices[i]] = dicionario
+
+    return grafo
+
+
+tamanho_grafo = int(input("Digite a quantidade de vértices de o grafo terá\n"))
 
 lista = input_data(tamanho_grafo)
+
+grafo = transforma_grafo(lista)
+
+ini = input("Qual é a origem camarada?\n")
+
+fim = input("Qual é o destino camarada?\n")
+
+if ini.upper() in grafo and fim.upper() in grafo:
+
+    dijkstra_path(grafo,ini.upper(),fim.upper())
+
+else:
+    print("mano tu é burro vai se fodeeeer faz o bagulho direito")
